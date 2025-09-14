06f5939eb29509052af30b73d2b7a53c89fc4d0d
ze-drummer <116662592+ze-drummer@users.noreply.github.com>
2025-09-14 18:03:23 +0200

    Description de ta modification

--- src/app.js
+++ src/app.js
@@ -1,24 +1,3 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const ordersRouter = require('./routes/orders');
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../public')));
-
-// Routes
-app.use('/api/orders', ordersRouter);
-
-// Démarrer le serveur
-app.listen(PORT, () => {
-    console.log(`Le serveur fonctionne sur http://localhost:${PORT}`);
-});
-
 document.addEventListener("DOMContentLoaded", () => {
   const dessertSection = document.getElementById("dessert-selection");
 
@@ -52,3 +31,5 @@ document.addEventListener("DOMContentLoaded", () => {
     button.disabled = true;
   });
 });
+
+
